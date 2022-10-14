00698e711fcfef8b4d06a13e15b08fe2598fb44c
Lilas Querrien <lilasquerrien@hotmail.fr>
2022-10-14 11:39:59 -0400

    Update

--- front/js/product.js
+++ front/js/product.js
@@ -1,26 +1,37 @@
 // Récuperer l'id dans l'URL du produit à afficher
-
 const url = new URLSearchParams(document.location.search);
 const productId = url.get('id')
 
 // Récupérer les données du produit dans l'API
-
 fetch("http://localhost:3000/api/products/" + productId)
+
 // Promesse  pour récupérer la réponse puis la transformer en json
-    .then(response => response.json())
-// Promesse  pour récupérer les data des différents produits
+    .then(response => 
+        response.json())
+// Promesse  pour modifier les datas des différents produits
     .then(data => {
         let image = document.querySelector(".item__img").innerHTML += `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
         let name = document.querySelector("#title").innerHTML += `<h1 id="title">${data.name}</h1>`;
         let price = document.querySelector("#price").innerHTML += `<span id="price">${data.price}</span>`;
         let description = document.querySelector("#description").innerHTML += `<p id="description">${data.description}}</p>`;
-// Boucle pour récupérer chacune des couleurs
-        let color = document.querySelector("#colors");
-        for (i = 0; i < data.colors.length; i++) {
-            color.innerHTML += `<option value="">${data.colors[i]}</option>`;
-        }
+// Boucle pour modifier les couleurs
+        let color = document.querySelector("#colors")
+            for (i = 0; i < data.colors.length; i++) {
+                color.innerHTML += `<option value="">${data.colors[i]}</option>`
+            }
     })
 // Message d'erreur
     .catch(error => {
-        alert("Erreur, veuillez recharger la page");
-    });
+        alert("Erreur, veuillez recharger la page")})
+
+// Ajouter les produits au panier
+
+// Définir const sélection quantité et couleur
+// Attention une couleur doit être choisie et une quantité entre 1 et 100
+// Ajouter un EventListener pour le clic sur le bouton
+// Le panier peut-être un array qui contient l'id, la quantité, la couleur
+// Accéder au local storage pour accéder à l'array depuis la page panier
+// Ajouter un produit à l'array lorsqu'il est ajouté au panier si pas déjà présent
+// Si déjà prédent alors alors on incrémente la quantité du produit correspondant
+// Ajouter au panier : continuer les achats ?
+// Attention la quantité totale par produit/couleur est de 100
--- front/js/script.js
+++ front/js/script.js
@@ -1,29 +1,27 @@
 // Récupérer les données de l'API
-
 fetch("http://localhost:3000/api/products")
+
 // Promesse  pour récupérer la réponse puis la transformer en json
-    .then(response => response.json())
+    .then(response => 
+      response.json())
 // Promesse  pour récupérer les data
     .then(data => {
-      displayProducts(data);
+      displayProducts(data)
+// Déclarer la fonction pour afficher les produits
+      function displayProducts(data) {
+// Boucle pour afficher chacun des produits de data
+        for (product of data) {
+// Trouver la section  #items dans index.html et modifier son contenu
+          let items = document.querySelector("#items").innerHTML += `<a href="./product.html?id=${product._id}">
+                                                                      <article>
+                                                                        <img src="${product.imageUrl}" alt="${product.altTxt}">
+                                                                          <h3 class="productName">${product.name}</h3>
+                                                                            <p class="productDescription">${product.description}</p>
+                                                                      </article>
+                                                                    </a>`;
+        }      
+      }
     })
 // Message d'erreur
     .catch(error => {
-      alert("Erreur, veuillez recharger la page");
-    });
-
-// Afficher les produits
-
-function displayProducts(data) {
-// Boucle pour chaque product de data
-  for (product of data) {
-// Trouver la section avec l'ID items dans index.html et modifier son contenu
-        let items = document.querySelector("#items").innerHTML += `<a href="./product.html?id=${product._id}">
-                                                                    <article>
-                                                                      <img src="${product.imageUrl}" alt="${product.altTxt}">
-                                                                      <h3 class="productName">${product.name}</h3>
-                                                                      <p class="productDescription">${product.description}</p>
-                                                                    </article>
-                                                                  </a>`;     
-  }      
-}
+      alert("Erreur, veuillez recharger la page")})
