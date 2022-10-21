ea620f70899e59f13d1cb010dd9f9167d247c9e0
Lilas Querrien <lilasquerrien@hotmail.fr>
2022-10-21 12:05:06 -0400

    21/10 page product

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,4 @@
-// Récuperer l'id du produit à afficher
+// Récuperer l'id du produit à afficher dans l'URL
 const url = new URLSearchParams(document.location.search);
 const productId = url.get('id')
 
@@ -25,9 +25,9 @@ function displayProductDetails(){
             price.innerHTML += `<span id="price">${data.price}</span>`;
             description.innerHTML += `<p id="description">${data.description}}</p>`;
 // Boucle pour modifier les couleurs
-        for (i = 0; i < data.colors.length; i++) {
-            color.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`
-        }})
+            for (i = 0; i < data.colors.length; i++) {
+                color.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`
+            }})
 // Message d'erreur
         .catch(error => {
             alert("Erreur, veuillez recharger la page!")
@@ -43,39 +43,43 @@ function storeProductDetails() {
 // Obliger le client à choisir une couleur
         if (color.value === "" ) {
             alert("Veuillez choisir une couleur!");
-        }
 // Obliger le client à choisir une quantité entre 1 et 100
-        if (quantity.value < 1 || quantity.value > 100 ) {
+        } else if (quantity.value < 1 || quantity.value > 100 ) {
             alert("Veuillez choisir une quantité d'article(s) entre 1 et 100!");
-        }
 // Message de confirmation d'ajout du produit au panier: continuer les achats?
-        if (color.value != "" && quantity.value >= 1 || quantity.value <= 100 ) {
-            alert(`Vous avez ajouté ${quantity.value} ${title.textContent} ${color.value} au panier, cliquez sur OK pour continuer vos achats!`);
-        }
+        } else { (color.value != "" && quantity.value >= 1 || quantity.value <= 100 ) 
+            alert(`Vous avez ajouté ${quantity.value} ${title.textContent} ${color.value} au panier, cliquez sur OK pour continuer vos achats!`)
 // Données à enregistrer dans le local storage
-    const customerSelection = {
-        id: productId,
-        color: color.value,
-        quantity: quantity.value,
-    }
+                const customerSelection = {
+                    id: productId,
+                    color: color.value,
+                    quantity: quantity.value,
+                }
 // Récuperer les données dans le local storage
-        let customerCart = JSON.parse(localStorage.getItem("Canapé"));
-// Si panier vide alors convertir JSON customerChoice en chaîne de charactères et stocker les données dans le local storage
-            if (!customerCart) {
-                customerCart = [];
-                customerCart.push(customerSelection);
-                localStorage.setItem("Canapé", JSON.stringify(customerCart));
-// Si déjà prédent alors alors on incrémente la quantité du produit correspondant
-            } else {
-
-
-
+    let customerCart = JSON.parse(localStorage.getItem("Canapé"));
+// Si le panier est vide alors on push les données de customerSelection dans le local storage
+        if (customerCart === null) {
+            customerCart = [];
+            customerCart.push(customerSelection);
+            localStorage.setItem("Canapé", JSON.stringify(customerCart));
+// Si le panier n'est pas vide: 2 options
+        } else {
+// Définir constante si le produit est déjà présent dans le panier: trouver par id et couleur
+            const alreadyInCart = customerCart.find(element => element.id == customerSelection.id && element.color == customerSelection.color);
+// Si le produit n'est pas déjà dans le panier alors on push les données
+                if (alreadyInCart == undefined) {
+                    customerCart.push(customerSelection);
+                    localStorage.setItem("Canapé", JSON.stringify(customerCart));
+// Si le produit (id+couleur identiques) est déjà prédent dans le panier alors on incrémente la quantité
+                } else {
+                    let newQuantity = parseInt(customerSelection.quantity) + parseInt(alreadyInCart.quantity);
+                    alreadyInCart.quantity = newQuantity;
+                    localStorage.setItem("Canapé", JSON.stringify(customerCart));
+                }  
             }
-
+        }
     })
 }
-    
-
 // Appel de la fonction
 storeProductDetails();
 
